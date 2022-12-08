ceb15a3b291ca98f9647e2c1dfb948c74f5def52
Oleksii Dembnovetskyi <dembnoveckii@gmail.com>
2022-12-08 23:09:17 +0100

    add styles

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
-// import './css/styles.css';
+const { Notify } = require('notiflix');
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-// const DEBOUNCE_DELAY = 300;
+const formEl = document.querySelector('.search-form');
+
+const KEY = '';
+
+const baseUrl = `https://pixabay.com/api/?key=${KEY}`;
+
+formEl.addEventListener('submit', event => {
+  event.preventDefault();
+  console.log(event.currentTarget.elements.searchQuery.value);
+  const searchText = event.currentTarget.elements.searchQuery.value;
+
+  `&q=${searchText}&image_type=photo&orientation=horizontal&safesearch=true`;
+});
+
+const sample = `<div class="photo-card">
+  <img src="" alt="" loading="lazy" />
+  <div class="info">
+    <p class="info-item">
+      <b>Likes</b>
+    </p>
+    <p class="info-item">
+      <b>Views</b>
+    </p>
+    <p class="info-item">
+      <b>Comments</b>
+    </p>
+    <p class="info-item">
+      <b>Downloads</b>
+    </p>
+  </div>
+</div>`;
